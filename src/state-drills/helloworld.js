6cfd80285763ca824a5bdf0943b2700888791630
Andrew Delarosa <andrewvdelarosa@gmail.com>
2020-08-26 09:06:41 -0700

    Hello world

---
+++ src/state-drills/helloworld.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { ReactComponent } from '*.svg';
+
+export default class HelloWorld extends React.Component {
+    state = {
+        greeting: "Test"
+    }
+
+    render() { 
+        return(
+            <div>
+                <p>Hello {this.state.greeting}</p>
+                <button onClick = {() => this.setState({ greeting: "World" }) }>World</button> 
+                <button onClick = {() => this.setState({ greeting: "Friend" }) }>Friend</button> 
+                <button onClick = {() => this.setState({ greeting: "React" }) }>React</button> 
+                </div>
+        )}
+    
+
+};
